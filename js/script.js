0adff20cc83ee7bd7e7956d5a07ad6a7579a318e
Leonel Oliveira <leoneloliver@gmail.com>
2016-10-16 13:12:20 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-
+function onYouTubeIframeAPIReady() {
+    var player;
+    player = new YT.Player('video', {
+        videoId: '1AzeIjQaZ58', // YouTube Video ID
+        playerVars: {
+        autoplay: 0, // Auto-play the video on load
+        controls: 1, // Show pause/play buttons in player
+        showinfo: 1, // Hide the video title
+        modestbranding: 1, // Hide the Youtube Logo
+        loop: 1, // Run the video in a loop
+        fs: 0, // Hide the full screen button
+        cc_load_policty: 0, // Hide closed captions
+        iv_load_policy: 3, // Hide the Video Annotations
+        autohide: 0 // Hide video controls when playing
+        },
+        events: {
+        onReady: function(e) {
+            e.target.mute();
+                var bplay = 0;
+                var bottom = $('.get_top_video_start').offset().top;
+                $(window).scroll(function(ev) {       
+                    if ($(this).scrollTop() > bottom && bplay == 0){
+                        player.playVideo();
+                        bplay = 1;
+                        ev.preventDefault(); 
+                    }
+                    
+                });
+            }
+        }
+    });
+}
